5112bd234fc4f4f1f75d39d94dc9dfc135bbd664
dleavitt14git config --global user.email ddleavitt14@gmail.com <ddleavitt14@gmail.com>
2019-01-20 00:38:17 -0700

    prove assignment02-test10

--- web/assignments.php
+++ web/assignments.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Assignments</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="" />
+    <link rel="stylesheet" type="text/css" media="screen" href="home.css" />
     
 </head>
 <body>
