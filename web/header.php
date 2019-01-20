a92ba7a716ee8901eca0563a25c7eb956303912f
dleavitt14git config --global user.email ddleavitt14@gmail.com <ddleavitt14@gmail.com>
2019-01-20 00:05:04 -0700

    prove assignment02

---
+++ web/header.php
@@ -0,0 +1,6 @@
+<nav>
+    <ul>
+        <li><a href="home.php">Home</li>
+        <li><a href="assignments.php">Assignments</li>
+    </ul>
+</nav>
---
+++ web/home.php
@@ -0,0 +1,25 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="home.css" />
+</head>
+<body>
+    <header>
+        <?php
+            include 'header.php';
+        ?>
+    </header>
+    <main>
+        <h1>The Eventer</h1>
+        <p>This is a great place to showcase some of the new things I am going to learn using PHP.</p>
+        <img src="horsePhoto.jpg" alt="Horse Jumping">
+    </main>
+    <footer>
+        <p>&copy Derek Leavitt 2018</p>
+    </footer>
+</body>
+</html>
