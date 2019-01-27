6de7ae4ba58b13b0509a1dfceeccc49e56818baf
dleavitt14git config --global user.email ddleavitt14@gmail.com <ddleavitt14@gmail.com>
2019-01-26 20:43:26 -0700

    shopping7

---
+++ web/PHP Shopping Cart/browse.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Browse Items</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+</head>
+<body>
+    <header>
+        <h1>The Pen Shop</h1>
+        <p id="view-cart"><a href="cart.php">View Cart</a></p>
+    </header>
+    <main>
+        <h2>Items For Sale</h2>
+        <section>
+            <div class="item">
+                <img src="images/blue-pen.jpg" alt="blue pen">
+                <h3>Blue Pen</h3>
+                <h4>$100</h4>
+            </div>
+            <div class="item">
+                <img src="images/multi-colored-pens.jpg" alt="multi colored pens">
+                <h3>Multi Colored Pens</h3>
+                <h4>$400</h4>
+            </div>
+            <div class="item">
+                <img src="images/orange-highlighter.jpg" alt="orange highlighter">
+                <h3>Orange Highlighter</h3>
+                <h4>$150</h4>
+            </div>
+            <div class="item">
+                <img src="images/red-pen.jpg" alt="red pen">
+                <h3>Red Pen</h3>
+                <h4>$100</h4>
+            </div>
+            <div class="item">
+                <img src="images/sharpies.jpg" alt="sharpies">
+                <h3>Sharpies</h3>
+                <h4>$500</h4>
+            </div>
+            <div class="item">
+                <img src="images/zebra-pen.jpg" alt="zebra pen">
+                <h3>Zebra Pen</h3>
+                <h4>$125</h4>
+            </div>
+            <img src="images/zebra-pen.jpg" alt="zebra pen">
+        </section>
+    </main>
+    <footer>
+        <p>&copy</p>
+    </footer>
+</body>
+</html>
