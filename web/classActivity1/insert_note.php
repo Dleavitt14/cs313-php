1f36c8c506f59f43a7c877b3bd68f97f0f0faaab
dleavitt14git config --global user.email ddleavitt14@gmail.com <ddleavitt14@gmail.com>
2019-02-14 11:16:36 -0700

    test8

--- web/classActivity1/insert_note.php
+++ web/classActivity1/insert_note.php
@@ -3,5 +3,17 @@ $course_id = htmlspecialchars($_POST['course_id']);
 $date = htmlspecialchars($_POST['date']);
 $content = htmlspecialchars($_POST['content']);
 
-echo $course_id . $date . $content;
+require_once("dbConnect.php");
+    $db = get_db();
+
+    $query = 'INSERT INTO note (date, content, course_id) VALUES(:date, :content, :course_id)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':date', $date, PDO::PARAM_STR);
+    $statement->bindValue(':content', $date, PDO::PARAM_STR);
+    $statement->bindValue(':course_id', $date, PDO::PARAM_INT);
+    $statement->execute();
+    $course = $statement->fetch(PDO::FETCH_ASSOC);
+
+header("Location:notes.php?course_id=$$course_id");
+die();
 ?>
