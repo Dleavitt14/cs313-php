81963906f2a71aa770791217f0d47f6a3012f7a5
dleavitt14git config --global user.email ddleavitt14@gmail.com <ddleavitt14@gmail.com>
2019-02-25 20:30:20 -0700

    event update7

--- web/fheProject/users.php
+++ web/fheProject/users.php
@@ -18,6 +18,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
     <title>Events</title>
 </head>
 <body>
